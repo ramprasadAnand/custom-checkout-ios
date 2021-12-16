cf84357ccaadb92b4026e4a779b3ccd7a818f7d4
Ramprasad A <85918255+ramprasadAnand@users.noreply.github.com>
2021-12-16 13:56:20 +0530

    Swift package added

---
+++ Tests/custom-checkout-iosTests/custom_checkout_iosTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import custom_checkout_ios
+
+final class custom_checkout_iosTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(custom_checkout_ios().text, "Hello, World!")
+    }
+}
